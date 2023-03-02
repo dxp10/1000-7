6274d534f919295226a36523eb510e447efb5d43
Joshua Rose <joshuarose099@gmail.com>
2023-03-02 21:27:23 +1100

    Update 1000-7.py

--- 1000-7 Python/1000-7.py
+++ 1000-7 Python/1000-7.py
@@ -1,5 +1,7 @@
 import time
 
-for n in range(1000, 7, -7): print(n); time.sleep(0.5)
+for n in range(1000, 7, -7):
+        print(f"{n+7} - 7 = {n}")
+        time.sleep(0.5)
         
 print('Я гуль')  # <-- Що означає це слово?
